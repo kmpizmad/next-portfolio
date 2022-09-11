c8fe10ea58d29afa6ee9ea0e07cc269e9c9aa1f8
kmpizmad <nagy.viktor392@gmail.com>
2022-09-11 23:51:12 +0200

    -- created IProject interface

--- src/interfaces/projects/IProject.ts
+++ src/interfaces/projects/IProject.ts
@@ -1,1 +1,12 @@
-export default interface IProject {}
+export default interface IProject {
+  name: string;
+  image: string;
+  repository: string;
+  type: ProjectType;
+}
+
+/**
+ * Web/Api: demo link
+ * Design: Carousel / Gallery
+ */
+type ProjectType = 'web' | 'design' | 'api';
