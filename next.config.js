da90f1db07aadf248e4897c72d2bcb60b0c668ce
kmpizmad <nagy.viktor392@gmail.com>
2022-09-11 23:26:42 +0200

    Integrated localization package -- next-i18next

--- next.config.js
+++ next.config.js
@@ -1,12 +1,14 @@
 const path = require('path');
+const { i18n } = require('./next-i18next.config');
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-	reactStrictMode: true,
-	swcMinify: true,
-	sassOptions: {
-		includePaths: [path.join(__dirname, 'src', 'styles')],
-	},
+  reactStrictMode: true,
+  swcMinify: true,
+  sassOptions: {
+    includePaths: [path.join(__dirname, 'src', 'styles')],
+  },
+  i18n,
 };
 
 module.exports = nextConfig;
