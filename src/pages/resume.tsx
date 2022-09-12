76ac1da99f27d2f3032f14a82aa26bfe2f9dbf06
kmpizmad <nagy.viktor392@gmail.com>
2022-09-12 09:12:01 +0200

    -- fixed linter issues + added TranslateButton

--- src/pages/resume.tsx
+++ src/pages/resume.tsx
@@ -2,6 +2,8 @@ import type { GetStaticPropsContext, NextPage } from 'next';
 // import styles from '../styles/modules/Resume.module.scss';
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
 import { useTranslation } from 'next-i18next';
+import TranslateButton from '../components/TranslateButton';
+import type { UnkownProps } from '../types/UnkownProps';
 
 const Resume: NextPage = () => {
   const { t: commonTranslator } = useTranslation('common');
@@ -17,12 +19,20 @@ const Resume: NextPage = () => {
 
   console.log({ education, certificates, skills, experience, languages, socials, info });
 
-  return <div>{commonTranslator('resume_title')}</div>;
+  return (
+    <div className="flex flex-col">
+      <div className="flex gap-2">
+        <TranslateButton targetLanguage="en" />
+        <TranslateButton targetLanguage="hu" />
+      </div>
+      <div>{commonTranslator('resume_title')}</div>
+    </div>
+  );
 };
 
 export default Resume;
 
-export async function getStaticProps(context: GetStaticPropsContext) {
+export async function getStaticProps(context: GetStaticPropsContext): Promise<UnkownProps> {
   return {
     props: {
       ...(await serverSideTranslations(context.locale || 'en', ['common', 'resume'])),
