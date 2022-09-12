61dd005eb94c3216b68d723e6f7d0dc24c3696f5
kmpizmad <nagy.viktor392@gmail.com>
2022-09-12 08:59:06 +0200

    -- added resume translations

--- src/pages/resume.tsx
+++ src/pages/resume.tsx
@@ -4,9 +4,20 @@ import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
 import { useTranslation } from 'next-i18next';
 
 const Resume: NextPage = () => {
-  const { t } = useTranslation('common');
+  const { t: commonTranslator } = useTranslation('common');
+  const { t: resumeTranslator } = useTranslation('resume');
 
-  return <div>{t('resume_title')}</div>;
+  const education = resumeTranslator('education', { returnObjects: true });
+  const certificates = resumeTranslator('certificates', { returnObjects: true });
+  const skills = resumeTranslator('skills', { returnObjects: true });
+  const experience = resumeTranslator('experience', { returnObjects: true });
+  const languages = resumeTranslator('languages', { returnObjects: true });
+  const socials = resumeTranslator('socials', { returnObjects: true });
+  const info = resumeTranslator('info', { returnObjects: true });
+
+  console.log({ education, certificates, skills, experience, languages, socials, info });
+
+  return <div>{commonTranslator('resume_title')}</div>;
 };
 
 export default Resume;
@@ -14,7 +25,7 @@ export default Resume;
 export async function getStaticProps(context: GetStaticPropsContext) {
   return {
     props: {
-      ...(await serverSideTranslations(context.locale || 'en', ['common'])),
+      ...(await serverSideTranslations(context.locale || 'en', ['common', 'resume'])),
     },
   };
 }
